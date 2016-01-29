62aab658a5b7e6b6723a4a6f54f9424bf6b7d328
Vittorio Mojoli <vittorio.mojoli@gmail.com>
2016-01-29 18:30:48 +0000

    My first cpp commit from Eclipse cpp

---
+++ HelloCppWorld/src/HelloCppWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloCppWorld.cpp
+// Author      : Vittorio Mojoli
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
